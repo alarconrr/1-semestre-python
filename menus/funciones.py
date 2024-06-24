673ec43c2e3b543aa8d2ae622478de047f1e5982
mysterion <168899495+alarconrr@users.noreply.github.com>
2024-06-24 19:07:45 -0400

    se arreglo el código.

--- menus/funciones.py
+++ menus/funciones.py
@@ -1,14 +1,94 @@
-def registrartrabajadores():
-    liquidopagar=(sueldoBruto-DescSalud-DescAfp);
+###Funciones Planilla
+import csv
+trabajadores=[];
 
-trabajador ={
- 'Nombre':nombre,
- 'Cargo':cargo,
- 'SueldoBruto':sueldoBruto,
- 'DescSalud':descSalud,
- 'DescAfp':descAfp,
- 'LiquidoPagar':liquidoapagar
-}
-trabajador.append(trabajador)
-print("Trabajador registrado con exito.");
-print(trabajadores);
+def registrarTrabajador():
+    while True:
+        try:
+            nombre=input("Ingrese el nombre y el apellido del trabajador:  ");
+        except ValueError:
+            print("error ingrese nuevamente el nombre y apellido.");
+        else:
+            try:
+                while True:
+                    cargo=input("Ingrese el cargo del trabajador (CEO), (Desarrollador) o (Analista):  ");
+                    if (cargo == "CEO") or (cargo == "Desarrollador") or (cargo == "Analista"):
+                        break;
+                    else:
+                        print ("Ingrese un cargo válido.");
+            except TypeError:
+                print("Error ingrese nuevamente el cargo.");
+            else:
+                try: 
+                    sueldoBruto=int(input("Ingrese el sueldo del trabajador:  "));
+                except ValueError:
+                    print("Error ingrese nuevamente el sueldo :<");
+                else:
+                    break;
+
+    if(cargo=='CEO'):
+        descSalud=sueldoBruto*0.07;
+        descAfp=sueldoBruto*0.12;
+    elif(cargo=='Desarrollador'):   
+        descSalud=sueldoBruto*0.05;
+        descAfp=sueldoBruto*0.10;
+    elif(cargo=='Analista'):
+        descSalud=sueldoBruto*0.06;
+        descAfp=sueldoBruto*0.11;
+
+    liquidoPagar=(sueldoBruto-descSalud-descAfp);
+
+    trabajador = {
+    'nombre': nombre,
+    'cargo': cargo,
+    'sueldoBruto': sueldoBruto,
+    'descSalud': descSalud,
+    'descAfp': descAfp,
+    'liquidoPagar': liquidoPagar
+    }
+    trabajadores.append(trabajador);
+    print("Trabajador registrado exitosamente.\n");
+    return;
+    
+def mostrarlista():
+    if not trabajadores:
+        print("no se encuentran trabajadores registrados 0-0");
+    else:
+        print("Listado de trabajadores:");
+        for trabajador in trabajadores:
+            print(f"Nombre: {trabajador['nombre']}, Cargo: {trabajador['cargo']}, Sueldo Bruto: {trabajador['sueldoBruto']},Descuento AFP: {trabajador['descAfp']}, Descuento salud: {trabajador['descSalud']} Líquido a Pagar: {trabajador['liquidoPagar']}");
+
+def imprimirPlanilla():
+    if not trabajadores:
+        print("No se encuentran trabajadores registrados.");
+        return;
+    
+    print("\n¿De quién desea imprimir la planilla?");
+    try:
+        decisionPlanilla = input("\n1.- CEO\n2.- Desarrollador\n3.- Analista\nIngrese su decisión: ");
+    except ValueError:
+        print ("Ingrese una opción válida");
+    else:
+        if decisionPlanilla == '1':
+            cargo = 'CEO';
+        elif decisionPlanilla == '2':
+            cargo = 'Desarrollador';
+        elif decisionPlanilla == '3':
+            cargo = 'Analista';
+        else:
+            print("Opción no válida");
+            return;
+
+        with open(f"Planilla_Trabajadores_{cargo}.txt", 'w') as planilla_trabajadores:
+            for trabajador in trabajadores:
+                if trabajador['cargo'] == cargo:
+                    planilla_trabajadores.write(f"Nombre: {trabajador['nombre']}, Cargo: {trabajador['cargo']}, Sueldo Bruto: {trabajador['sueldoBruto']}, Desc. Salud: {trabajador['descSalud']}, Desc. AFP: {trabajador['descAfp']}, Líquido a Pagar: {trabajador['liquidoPagar']}\n");
+                    print(f"Planilla de sueldos para el cargo {cargo} generada exitosamente.");
+        
+        print(f"\nContenido de la planilla para el cargo {cargo}:\n");
+        try:
+            with open(f"Planilla_Trabajadores_{cargo}.txt", 'r') as planilla_trabajadores:
+                contenido = planilla_trabajadores.read();
+                print(contenido);
+        except FileNotFoundError:
+            print(f"No se encontró la planilla para el cargo {cargo}. Verifique que haya sido generada previamente.");
--- menus/menus.py
+++ menus/menus.py
@@ -1,54 +1,31 @@
-#Menu bienvenida
-import funciones 
+###Empresa DUOC
+import funciones as func
+import time
+trabajadores=[];
+
+
 while True:
-    print("********************************");
-    print("Bienvenido al menu empresa duoc");
-    print("********************************");
-    print("1-.Ingresar el trabajador.");
-    print("2-.Listar todos los trabajadores.");
-    print("3-.Imprimir planilla de sueldos.");
-    print("4-.Salir del programa.");
+    print("\nBienvenido a DUOC Empresa\n\nElija una opción:  ");
+    print("1.- Registrar Trabajador");
+    print("2.- Lista de trabajadores");
+    print("3.- Imprimir planilla de sueldos");
+    print("4.- Salir del programa");
+
     try:
-        opc=int(input("Ingrese una opcion-->"));
+        menuDecision = int(input("\nIngrese una opción: "));
     except ValueError:
-        print("Error ingrese un nunero del 1 al 4.");
+        print("Error ingrese nuevamente. :(");
     else:
-        if (opc==1):
-            while True:
-                try:
-                    nombre=print("Ingrese el nombre del trabajador.")
-                    input("-->");
-                    cargo=print("Ingrese el cargo del trabajador.");
-                    input("-->");
-                    sueldo_Bruto=print("Ingrese el sueldo bruto del trabajador.");
-                    input("-->");
-                    descSalud=print("Ingrese el descuento de salud del trabajador.");
-                    input("-->");
-                    descAfp=print("Ingrese el descuento de la afp del trabajador.");
-                    input("-->");
-                    liquidoapagar=print("Ingrese el saldo liquido a pagar del trabajador.");
-                    input("-->");
-                    with open("datostrabajadores.txt",'w') as file:
-                        file.write(f"{nombre}");
-                        file.write(f"{cargo}");
-                        file.write(f"{sueldo_Bruto}");
-                        file.write(f"{descSalud}");
-                        file.write(f"{descAfp}");
-                        file.write(f"{liquidoapagar}");
-                except ValueError:
-                    print("Error ingrese los datos nuevamente.");
-                    break;
-                else:
-                     if (opc==2):
-                         while True:
-                                try:
-                                    print(f"la lista es {trabajador}:");
-                                except ValueError:
-                                 print("Error ingrese los datos nuevamente.");
-                                else:
-                                    if (opc==3):
-                                        while True:
-                                             try:
-                                                 print(f"{trabajador}");
-                                             except ValueError:
-                                                 print("Error ingrese los datos nuevamente.");
+        if (menuDecision == 1):
+            func.registrarTrabajador();
+            trabajadores.append(trabajadores);
+        elif (menuDecision == 2):
+            func.mostrarlista();
+        elif (menuDecision == 3):       
+            func.imprimirPlanilla();
+        elif (menuDecision == 4):
+            print ("Saliendo...");
+            time.sleep(2);
+            break;
+        else:
+            print("Error ingrese nuevamente :<");
